9d59292c27aa307487cef6a0ac218956caadd7a3
Ivo <ivothaielvicencio@gmail.com>
2025-04-11 11:27:30 -0300

    Realización del ejercicio 1

---
+++ js/ej1.js
@@ -0,0 +1,10 @@
+//declaramos la constantes con los nombres del grupo
+const integrantes = "Lautaro, Ivo, Maxi, Alex, Matias"
+
+
+//mostramos el mensaje usando literales de plantilla para sintaxis mas limpia
+console.log (`El grupo esta formado por: ${integrantes}`);
+
+//${} sirve para que 
+//se pueda concatenar el texto con la variable
+//si se encuentra una variable dentro de la cadena de texto, se reemplaza con el valor
