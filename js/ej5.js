6e1ff2f4c7f624b1dfb67992638c4c6238ce39b9
Ivo <ivothaielvicencio@gmail.com>
2025-04-11 12:20:17 -0300

    Realización del ejercicio 5

---
+++ js/ej5.js
@@ -0,0 +1,9 @@
+//declarar dos variables, asignarles valor y realizar su conversion  a seg
+const horas = 2;
+const minutos = 30;
+
+const segundos = (horas * 3600) + (minutos * 60);
+
+//mostramos
+console.log(`Horas: ${horas} Minutos: ${minutos}`);
+console.log(`Segundos: ${segundos}`);
