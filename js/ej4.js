03fd3766c6f9d5154847a0f3270b71190147eaf7
Ivo <ivothaielvicencio@gmail.com>
2025-04-11 12:13:53 -0300

    Realizacion del ejercicio 4

---
+++ js/ej4.js
@@ -0,0 +1,14 @@
+//area y perimetro de un cuadrado
+
+
+//declaramos un lado como constante
+const lado = 5;
+
+//calculamos el area
+const area = lado * lado;
+
+//calculamos el perimetro
+const perimetro = lado * 4;
+
+//mostramos
+console.log(`El area del cuadrado es ${area} y el perimetro es ${perimetro} `);
